6efc0b132c4d2380dc598edfe2b5cbb645ef83be
Jorge <ardonz@gmail.com>
2020-05-29 11:34:21 -0600

    Initial deploy

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from '../pages/HomePage';
+
+import '../styles/styles.scss'
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from '../components/Directory';
+
+import '../styles/homepage.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
+
+export default HomePage;
